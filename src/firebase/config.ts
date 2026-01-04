418ef003e9fc4bd7eab783bbadfea400acc9b19d
Olivia Hill <hill.ol@northeastern.edu>
2026-01-03 19:57:06 -0500

    fixing api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -9,14 +9,14 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAftzsN1ovhkwZPnCy420y2uS1NMqc5Kz8",
-    authDomain: "co-op-tracker-6e9c3.firebaseapp.com",
-    databaseURL: "https://co-op-tracker-6e9c3-default-rtdb.firebaseio.com",
-    projectId: "co-op-tracker-6e9c3",
-    storageBucket: "co-op-tracker-6e9c3.firebasestorage.app",
-    messagingSenderId: "694687300884",
-    appId: "1:694687300884:web:678d6beabebf6836581601",
-    measurementId: "G-MYFMZKSX1S"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
